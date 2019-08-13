9d9d63aeb612f4a76bcf8dd6418527f512614709
Diana Grams <deegrams221@gmail.com>
2019-08-12 20:54:52 -0600

    fixed button display

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,15 +1,15 @@
 import React, {useState} from "react";
 
 //import any components needed
-import Display from "../../DisplayComponents/Display";
 import NumberButton from "./NumberButton";
 
 //Import your array data to from the provided data file
 import {numbers} from "../../../data";
 
-const Numbers = (props) => {
+const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [numberState, newNumberState] = useState(numbers);
+  //console.log(numberState);
   return (
     <div className = "numberButtonContainer">
       {numberState.map((number, index) => {
